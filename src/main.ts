84abed0841789d73fe8be5ad314b419266dc3557
Rbh <mikung555@gmail.com>
2026-05-07 10:56:01 +0700

    change port to 3002

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,6 @@ async function bootstrap() {
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3002);
 }
 bootstrap();
